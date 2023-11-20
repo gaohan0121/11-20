76d3b3d4203bd387f957cf8c32fa5ba8d0d0040a
gaohan0121 <150786254+gaohan0121@users.noreply.github.com>
2023-11-20 15:13:59 +0800

    Add files via upload

---
+++ C/test-11-13/max.c
@@ -0,0 +1,5 @@
+int Max(int a,int b)
+{
+    return a>b? a:b;
+    return 0;
+}
---
+++ C/test-11-13/test.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+#include "max.h"//引用自己的头文件
+
+int main()
+{
+    int a=10;
+    int b=20;
+    int c=Max(a,b);
+    printf("hello world\n");
+    printf("%d\n",c);
+    
+    return 0;
+}
